022713d7e313e16937356ed886e0706ae69ae18b
SemionStepan <95352195+SemionStepan@users.noreply.github.com>
2023-06-01 20:08:32 +0300

    Function implementations added

--- studentSorter/main.cpp
+++ studentSorter/main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main()
 {
+    //массив студентов
     //главный цикл
     return 0;
 }
--- studentSorter/studentSorter.cpp
+++ studentSorter/studentSorter.cpp
@@ -1,7 +1,15 @@
 #include "studentSorter.h"
 
-int exec(Student student)
+int exec(Student student[10])
 {
-
+    int n = 0;
+    //создать список
+    while(n < 10)
+    {
+        //поместить в список
+        n++;
+        //отсортировать список
+    }
+    //printStudents(student);
     return 0;
 }
